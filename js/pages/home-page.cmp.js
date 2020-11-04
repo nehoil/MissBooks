eb1ace5db5b89608829b6f10460a17e44d4dfb5a
Nehoray Ilani <neho12@gmail.com>
2020-11-04 11:27:23 +0200

    second commit

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -4,7 +4,8 @@ export default {
         <section class="book-app">
             <section class="home-page-header">
                 <div class="home-page-header-container">
-                    <h1>Welcome to Bookify!</h1>
+                    <p>Go</p>
+                    <p>Read.</p>
                  </div>
             </section>
         </section>
